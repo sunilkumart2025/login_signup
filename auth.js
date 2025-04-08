4d99494eb3d3cc139e67a815de9e890b2b9f1bc2
sunilkumar_25 <suniltamil2025@gmail.com>
2025-04-08 20:46:09 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // auth.js
-const supabaseUrl = 'https://xyz.supabase.co'; // replace with your URL
-const supabaseKey = 'your-anon-key'; // replace with your anon key
+const supabaseUrl = 'https://aafmgvxhpdjsqrbjkzps.supabase.co'; // replace with your URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhZm1ndnhocGRqc3FyYmprenBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxMTE2NjEsImV4cCI6MjA1OTY4NzY2MX0.0uxlvXQ-aX5Jk8ICsird0SCaDPgbh5NQkAI6UGIxj1Q'; // replace with your anon key
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 document.addEventListener('DOMContentLoaded', async () => {
